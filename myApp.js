7a219e28267e18e17eb463153bbf255517e7c1a6
ronaldcnunez <Ronaldcuello11@gmail.com>
2018-12-09 16:02:06 -0500

    First lesson update

--- myApp.js
+++ myApp.js
@@ -1,6 +1,21 @@
 
 var express = require('express');
 var app = express();
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
 
 // --> 7)  Mount the Logger middleware here
 
